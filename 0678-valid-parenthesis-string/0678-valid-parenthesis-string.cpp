51ba766f669622bf75b21d92bdb54775a9c1965a
kanishk3114S <chesssynid@gmail.com>
2026-03-02 23:38:57 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.39%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        
+        //for every '*' .. we will maintain the minimum possible answer and maximum possible answer//
+
+        int min = 0 , max = 0;
+
+        for (char ch:s) {
+
+            if (ch == '(') {
+                min++;
+                max++;
+            } else if (ch == ')') {
+                min--;
+                max--;
+            } else { //asteric founded//
+                
+                min--;
+                max++;
+
+            }
+
+        if (min < 0) min = 0;
+        if (max < 0) return false; //till that point the max is negative.....tf its not possible//
+
+        }
+
+        if (min == 0) return true;
+
+        return false;
+
+    }
+};
