f35385af47a150fd223a3e44eb8b8cb2b3d093ca
kanishk3114S <chesssynid@gmail.com>
2026-03-22 11:19:43 +0530

    Time: 0 ms (100%), Space: 14.8 MB (20.77%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-
-    bool isequal(vector<vector<int>>& mat, vector<vector<int>>& targ) {
-
-        for (int i=0; i<mat.size(); i++) {
-            for (int j=0; j<mat[0].size(); j++) {
-                if (mat[i][j] != targ[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-
-    }
-
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& targ) {
         
-        if (isequal(mat,targ)) return true;
         int n = mat.size();
         // cout << n;
 
         //for first rotation//
-        bool r1 = true , r2 = true , r3 = true;
+        bool r0 = true ,  r1 = true , r2 = true , r3 = true;
 
         for (int i=0; i<mat.size(); i++) {
             for (int j=0; j<mat[0].size(); j++) {
-
+                
+                if (mat[i][j] != targ[i][j]) r0 = false;
                 if (mat[i][j] != targ[j][n-i-1]) r1 = false;
                 if (mat[i][j] != targ[n-1-i][n-1-j]) r2 = false;
                 if (mat[i][j] != targ[n-1-j][i]) r3 = false;
@@ -34,8 +20,8 @@ public:
         }
 
 
-        if (r1 || r2 || r3) return true;
-        else if (!r1  && !r2 && !r3) return false;
+        if (r1 || r2 || r3 || r0) return true;
+        else if (!r1  && !r2 && !r3 && !r0) return false;
         
         return false;
     }
