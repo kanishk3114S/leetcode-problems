2f903ca65275ad7c2dfd27c36223dcfd546843b4
kanishk3114S <chesssynid@gmail.com>
2026-03-25 21:30:14 +0530

    Time: 10 ms (80%), Space: 15.5 MB (89.94%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,50 @@
+    /*
+    // Definition for a Node.
+    class Node {
+    public:
+        int val;
+        vector<Node*> children;
+
+        Node() {}
+
+        Node(int _val) {
+            val = _val;
+        }
+
+        Node(int _val, vector<Node*> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+    */
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        
+        vector<int> result;
+        stack<Node*> s;
+
+        if (root == NULL) return result;
+
+        s.push(root);
+
+        while (!s.empty()) {
+
+            Node* node = s.top();
+            s.pop();
+            
+            for (int i=node->children.size()-1; i>-1; i--) { //start traversing from the right to the left//
+
+                s.push(node->children[i]);
+                
+            }
+
+            result.push_back(node->val);
+
+        }
+        return result;
+
+    }
+
+};
