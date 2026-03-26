2a89a496366cec37fdf85990f6506e778401d773
kanishk3114S <chesssynid@gmail.com>
2026-03-27 01:35:30 +0530

    Time: 1 ms (27.94%), Space: 23.7 MB (72.29%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,40 +11,29 @@
  */
 class Solution {
 public:
-  int geth(TreeNode* root) { //O(N) time complexity//
+    int getHeight(TreeNode* root , int& maxh) { 
 
-    if (root == NULL) return 0;
+        if (root == NULL) return 0;
 
-    int l = geth(root->left);
-    int r = geth(root->right);
+        int left = getHeight(root->left , maxh);
+        int right = getHeight(root->right, maxh);
+        maxh = max(maxh ,left+right);
 
-    return 1+max(l,r);
 
-  }
+        //at every node--> we have left and right which gives us lefth and righth//
+        // but we used to calculate the 1+max(left,right) because for root that is the height//
+        //and while getting the answer this happens with every node//
 
-void dfs(TreeNode*root , int& ans) {
+        //so lh+rh maintain karrrrrrrrrrrrrrrrrrrlo//
 
-    if (root == NULL) return;
+        return 1+max(left,right);
 
-    //at every node//
 
-    int left = geth(root->left); //we get the left height//
-    int right = geth(root->right); // we get the right height//
-
-    int diameter = left+right;
-
-    ans = max(diameter , ans);
-
-    //traverse the tree//
-
-    dfs(root->left ,ans);
-    dfs(root->right , ans);
-
-}
+    }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        int ans = INT_MIN;
-        dfs(root , ans);
-        return ans;
+        int maxh = 0;
+        getHeight(root , maxh);
+        return maxh;
     }
 };
