ac10e26365cd4e81ba4c9aed8dfa042bb6f142fe
kanishk3114S <chesssynid@gmail.com>
2026-03-28 16:57:54 +0530

    Time: 48 ms (55.83%), Space: 29.1 MB (45.45%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        
+        //make 3 pointers i,j,k.....//
+
+        int i = 0 , j = i+1 , k=arr.size()-1;
+
+        vector<vector<int>> ans;
+        sort(arr.begin() , arr.end());
+        int sum = 0;
+        
+        while (i<arr.size()) {
+        while (i>0 && i<arr.size() && arr[i] == arr[i-1]) {
+                    i++;
+           }
+           j = i+1;
+           k = arr.size()-1;
+            while (j < k) {
+                sum = arr[i]+arr[j]+arr[k];
+                if (sum == 0) {
+                    
+                    ans.push_back({arr[i] , arr[j] , arr[k]});
+                    j++;
+                    k--;
+                while (j < arr.size() && arr[j] == arr[j-1]) {
+                    j++;
+                }
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+            i++;
+
+        }
+    return ans;
+    }
+};
