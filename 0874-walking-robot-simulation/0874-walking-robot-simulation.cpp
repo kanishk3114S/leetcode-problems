22c69847ff19cf6c4a2f53385b3493d4dba5d4d9
kanishk3114S <chesssynid@gmail.com>
2026-04-07 00:04:53 +0530

    Time: 20 ms (76.53%), Space: 37.6 MB (75.47%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int robotSim(vector<int>& cmd, vector<vector<int>>& obstacles) {
+        // maintain a set for the obtacles//
+
+        set<pair<int,int>> s;
+
+        for (auto&o:obstacles) s.insert({o[0] , o[1]});
+
+        vector<pair<int,int>> dir = {
+
+            {0,1} , //north
+            {1 , 0}, //east
+            {0 , -1}, //south 
+            {-1 , 0} //west
+
+        };
+
+        //we will follow a structred simulation...earlier i was jumping to obtacles...but lets move one by one as obtacles is the point not the range//
+        int d = 0; //index for the directions....initially it was 0//
+        int x = 0 , y = 0; //absolute positions//
+        int ans = 0;
+        for (int i=0; i<cmd.size(); i++) {
+
+            if (cmd[i] == -1) {
+                d = (d+1)%4; //shift right
+            } else if (cmd[i] == -2) {
+                d = (d+3)%4; //shift left
+            } else {
+
+                for (int steps = 0; steps < cmd[i]; steps++) {
+
+                    int dx = x + dir[d].first;
+                    int dy = y + dir[d].second;
+
+                    if (s.count({dx , dy})) break;
+
+                    x  = dx;
+                    y  = dy;
+
+                    
+                    ans = max(ans , x*x+y*y);
+
+                }
+
+            }
+
+        }
+
+    return ans;
+
+    }
+};
