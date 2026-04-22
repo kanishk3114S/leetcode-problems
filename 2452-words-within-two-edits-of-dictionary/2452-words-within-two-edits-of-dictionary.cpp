602dce5ac45f2fa89bbd0baefce480732a5efc7a
kanishk3114S <chesssynid@gmail.com>
2026-04-22 14:19:46 +0530

    Time: 47 ms (5.35%), Space: 31.4 MB (15.51%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+bool isgood(string s , string t) {
+
+    int changed = 0;
+
+    for (int i=0; i<s.length(); i++) {
+
+        if (s[i] != t[i]) changed++;
+
+    }
+
+    if (changed <=2) return true;
+
+    return false;
+
+}
+
+vector<string> twoEditWords(vector<string>& q, vector<string>& d) {
+    
+    vector<string> ans;
+    for (int i=0; i<q.size(); i++) {
+        string s = q[i];
+        for (int j=0; j<d.size(); j++) {
+            string t = d[j];
+            if (isgood(s , t)) {
+
+                ans.push_back(s);
+                break;
+
+            }
+        }
+    }
+    return ans;
+
+}
+};
