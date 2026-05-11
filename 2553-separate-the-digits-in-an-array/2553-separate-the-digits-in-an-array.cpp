9ddd66af74f39b2e675ba202b5f832b34864448e
kanishk3114S <chesssynid@gmail.com>
2026-05-11 15:32:58 +0530

    Time: 0 ms (100%), Space: 15.2 MB (77.65%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+
+        for(int i=0; i<nums.size(); i++) {
+
+            string s = to_string(nums[i]);
+
+            for (int i=0; i<s.length(); i++) {
+
+                ans.push_back(s[i]-'0');
+
+            }
+
+        }
+        return ans;
+    }
+};
