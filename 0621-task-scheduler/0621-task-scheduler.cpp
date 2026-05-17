b6375c9d7f963f6fc1362b244e1dea1c89b9a403
kanishk3114S <chesssynid@gmail.com>
2026-05-18 03:56:21 +0530

    Time: 1929 ms (5.01%), Space: 459.3 MB (5.04%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        
+        unordered_map<char,int> mpp;
+        for (int i=0; i<tasks.size(); i++) {
+
+            mpp[tasks[i]]++;
+
+        }
+
+        using pp = pair<int,char>;
+        priority_queue<pp> pq;
+
+        for(auto it:mpp) {
+
+            pq.push({it.second , it.first});
+
+        }
+
+        unordered_map<char,int> hash;
+        for (auto it:mpp) {
+            hash[it.first] = 1; //initially minimum seat number would be 1//
+        }
+
+        int seat = 1;
+
+        while (!pq.empty()) {
+
+        vector<pp> corner;
+
+        while (!pq.empty()) {
+
+            int freq = pq.top().first;
+            char ch = pq.top().second;
+            pq.pop();
+
+            if (hash[ch] <= seat) {
+                freq--;
+                if (freq > 0) {
+                pq.push({freq , ch});    
+                }
+                hash[ch] = seat+1+n;
+                break;
+            } else {
+                corner.push_back({freq,ch});
+            }
+
+        }
+
+        for (int i=0; i<corner.size(); i++) {
+            pq.push(corner[i]);
+        }
+        seat++;
+        corner.empty();
+
+        }
+
+        return seat-1;
+        
+
+    }
+};
