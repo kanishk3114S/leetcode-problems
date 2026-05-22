9beeb34af5a728993519ccf9a9a6ec20e474f8f9
kanishk3114S <chesssynid@gmail.com>
2026-05-23 01:36:44 +0530

    Time: 23 ms (83.82%), Space: 16.6 MB (61.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,64 +1,40 @@
 class Solution {
 public:
 
-    void dfs(pair<int,int> a,
-             vector<vector<vector<pair<int,int>>>>& adj,
-             vector<vector<bool>>& vis) {
+    void dfs(int i , int j , vector<vector<char>>& grid , vector<vector<bool>>&vis) {
 
-        vis[a.first][a.second] = true;
-
-        for (auto &p : adj[a.first][a.second]) {
-
-            if (!vis[p.first][p.second]) {
-                dfs(p, adj, vis);
-            }
+        int n = grid.size() , m = grid[0].size();
+        if (i < 0 || j<0 || i>=n || j>=m || grid[i][j] == '0' || vis[i][j]) return;
+        if (grid[i][j] == '1') {
+            vis[i][j] = true;
         }
+
+        dfs(i+1 ,j , grid , vis); //down
+        dfs(i , j-1 , grid , vis);//left;
+        dfs(i , j+1 , grid , vis); //right
+        dfs(i-1 , j , grid , vis); //upper
+
     }
 
     int numIslands(vector<vector<char>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-
-        vector<vector<vector<pair<int,int>>>> adj(
-            n,
-            vector<vector<pair<int,int>>>(m)
-        );
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                if (grid[i][j] == '1') {
-
-                    if (j > 0 && grid[i][j-1] == '1')
-                        adj[i][j].push_back({i, j-1});
-
-                    if (i > 0 && grid[i-1][j] == '1')
-                        adj[i][j].push_back({i-1, j});
-
-                    if (j < m-1 && grid[i][j+1] == '1')
-                        adj[i][j].push_back({i, j+1});
-
-                    if (i < n-1 && grid[i+1][j] == '1')
-                        adj[i][j].push_back({i+1, j});
-                }
-            }
-        }
-
+        
+        int n = grid.size() , m = grid[0].size();
+        vector<vector<bool>> vis(n , vector<bool>(m , false));
         int cnt = 0;
+        for (int i=0; i<n; i++) {
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            for (int j=0; j<m; j++) {
 
                 if (grid[i][j] == '1' && !vis[i][j]) {
                     cnt++;
-                    dfs({i, j}, adj, vis);
+                    dfs(i , j , grid , vis);
                 }
+
             }
+
         }
 
         return cnt;
+
     }
 };
