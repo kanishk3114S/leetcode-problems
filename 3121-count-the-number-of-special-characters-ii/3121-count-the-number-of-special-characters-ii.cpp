c18cd0df8dd77e24d5db79eda3351f719c74a427
kanishk3114S <chesssynid@gmail.com>
2026-05-26 05:51:34 +0530

    Time: 67 ms (72.1%), Space: 24.2 MB (89.78%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    struct D {
+        bool lower;
+        bool upper;
+        int lastocc;
+        int firstocc;
+    };
+
+    int numberOfSpecialChars(string s) {
+        
+        vector<D> check(26 , {false , false , -1 , -1});
+
+        for (int i=0; i<s.length(); i++) {
+
+            if (islower(s[i])) {
+
+                check[s[i]-'a'].lower = true;
+                check[s[i] - 'a'].lastocc = i;
+
+            } else {
+
+                check[s[i] - 'A'].upper = true;
+                if (check[s[i]-'A'].firstocc == -1) {
+                    check[s[i] - 'A'].firstocc = i;
+                }
+
+
+            }
+
+        }
+
+        int ans = 0;
+
+        for (int i=0; i<check.size(); i++) {
+
+            int l = check[i].lower;
+            int r = check[i].upper;
+            int a = check[i].lastocc;
+            int b = check[i].firstocc;
+
+            if (l && r && a < b) ans++;
+
+        }
+
+        return ans;
+
+
+    }
+};
