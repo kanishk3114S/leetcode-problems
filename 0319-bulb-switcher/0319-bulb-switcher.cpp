1c0f73541038d2ebee4856010a2b938bdb846238
kanishk3114S <chesssynid@gmail.com>
2026-06-12 19:25:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (1.98%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        
+        //concept---> every ithe bulb will get toggle its factors times//
+        // if it has odd factors --> remain on//
+        // therefore only perfect squares has odd factors//
+
+        return sqrt(n);
+
+    }
+};
