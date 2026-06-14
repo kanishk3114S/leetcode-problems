10bb44bcf99b2d4db8a257fdfbc3e4d87706ab1e
kanishk3114S <chesssynid@gmail.com>
2026-06-15 00:54:27 +0530

    Time: 3 ms (12.4%), Space: 12.6 MB (24.96%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        
+        unordered_map<char,int> mpp;
+
+        int idx = 0;
+
+        for (char ch:order) {
+
+            mpp[ch] = idx;
+            idx++;
+
+        }
+
+
+
+        for (int i=0; i<words.size()-1; i++) {
+
+            string s1 = words[i];
+            string s2 = words[i+1];
+
+            if (s1.length() > s2.length() && s1.substr(0,s2.length()) == s2) return false;
+
+            for (int i=0; i<min(s1.length() , s2.length()); i++) {
+
+                if (s1[i] != s2[i]) {
+
+                    // cout << s1[i] << " " << s2[i] << " " << mpp[s1[i]] << " " << mpp[s2[i]];
+
+                    if (mpp[s1[i]] > mpp[s2[i]]) return false;
+                    else break;
+
+                } else {
+                    continue;
+                }
+
+            }
+        }
+
+        return true;
+
+    }
+};
