1893e9b9e8aa7e7d447b4b84fdb394fad98374be
kanishk3114S <chesssynid@gmail.com>
2026-06-20 23:19:08 +0530

    Time: 99 ms (11.11%), Space: 112.4 MB (8.33%) - LeetHub

--- 1840-maximum-building-height/1840-maximum-building-height.cpp
+++ 1840-maximum-building-height/1840-maximum-building-height.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
     int maxBuilding(int n, vector<vector<int>>& rest) {
+        
+        //instead of building the vector<int> heights [result=MLE]//
+        // we will move between the restricted areas and we will see the max height between them.//
         using ll = long long;
-        rest.push_back({1, 0});
-        sort(rest.begin(), rest.end());
-
-        vector<pair<ll, ll>> h(rest.size());
+        rest.push_back({1,0});
+        sort(rest.begin() , rest.end());
+        vector<pair<ll,ll>> h(rest.size() , {-1,-1});
         for (int i = 0; i < rest.size(); i++) {
             h[i] = {rest[i][0], rest[i][1]};
         }
 
-        for (int i = 0; i < h.size() - 1; i++) {
-            ll dist = h[i+1].first - h[i].first;
-            h[i+1].second = min(h[i+1].second, h[i].second + dist);
+        int maxht = INT_MIN;
+
+
+        for (int i=0; i<rest.size()-1; i++) {
+
+            int dist = rest[i+1][0]-rest[i][0];
+
+            h[i+1].second = min(h[i+1].second , h[i].second + dist);
+
         }
 
-        for (int i = h.size() - 1; i > 0; i--) {
-            ll dist = h[i].first - h[i-1].first;
-            h[i-1].second = min(h[i-1].second, h[i].second + dist);
+        //backward//
+
+        for (int i=rest.size()-1; i>0; i--) {
+
+            int dist = rest[i][0]-rest[i-1][0];
+
+            h[i-1].second = min(h[i-1].second , h[i].second + dist);
+
         }
 
-        ll maxpeak = 0;
-        for (int i = 0; i < h.size() - 1; i++) {
-            ll dist = h[i+1].first - h[i].first;
-            ll peak = (h[i].second + h[i+1].second + dist) / 2;
-            maxpeak = max(peak, maxpeak);
+        int maxpeak = 0;
+
+        for (int i=0; i<h.size()-1; i++) {
+
+            int dist = h[i+1].first-h[i].first;
+            int peak = (h[i].second+h[i+1].second+dist)/2;
+
+            maxpeak = max(peak , maxpeak);
+
+
         }
 
         ll final_dist = n - h.back().first;
         ll final_peak = h.back().second + final_dist;
-        maxpeak = max(maxpeak, final_peak);
+        maxpeak = max((long long)maxpeak, final_peak);
 
         return maxpeak;
+
     }
 };
