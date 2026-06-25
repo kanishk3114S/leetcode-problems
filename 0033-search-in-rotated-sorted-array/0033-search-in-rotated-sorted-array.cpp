a0f9b833292cc08e4d3097c0a372a48f91e27eb2
Kanishk Sharma <chesssynid@gmail.com>
2026-06-25 05:31:26 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-int search(vector<int>& nums , int target) { 
-    int len = nums.size();
-    int start = 0; 
-    int end = len-1;
-    while (start<=end) { 
-        int mid = start +(end-start)/2;
-        if (nums[mid] == target) { 
-            return mid;
-        }
-        //left array is sorted//
-        if (nums[start] <= nums[mid]) {
-            //check whether the target is there or not//
-            if (target>=nums[start] && target < nums[mid]) {
-                end = mid - 1;
-            } else { 
-                start = mid+1; //that means target = unsorted array//
-                //Now unsorted array = sorted array + unsorted array (coz middle value in that array divides the further arrays)//
-                //.. Repeat the same for that array as well//
-            }
-        } 
+    int search(vector<int>& arr, int target) {
         
-        //right array is sorted// 
-        else { 
-            if (target >  nums[mid] && target <= nums[end]) {
-                start = mid + 1;
+        //at every cut...either left will be sorted or right will be sorted//
+
+        int low = 0 , high = arr.size()-1;
+
+        while (low <= high) {
+
+            int mid = low - (low-high)/2;
+            if (arr[mid] == target) return mid;
+            if (arr[low] <= arr[mid]) {
+                //left half is sorted//
+
+                if (arr[low] <= target && arr[mid] >= target) {
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+
             } else {
-                end = mid-1; //array is in left unsorted array...//
+
+                if (arr[mid] <= target && arr[high] >= target) {
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+
             }
         }
+
+        return -1;
+
     }
-    return -1;
-}
 };
