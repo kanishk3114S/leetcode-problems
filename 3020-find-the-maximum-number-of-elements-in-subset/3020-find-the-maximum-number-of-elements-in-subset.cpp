46aad6413dd00c62da4b51ee96e63d5d9ed34f63
Kanishk Sharma <chesssynid@gmail.com>
2026-06-27 06:53:31 +0530

    Time: 213 ms (8%), Space: 135.1 MB (11.43%) - LeetHub

---
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+
+    /*
+    
+    x , x2 , x4 , xpeak , xpeak/2 xpeak/2......;
+    so its like peak....for every x another x should exists
+    
+    */
+
+    int maximumLength(vector<int>& arr) {
+        
+        using ld = long double;
+
+        unordered_map<ld,ld> mpp;
+
+        for (auto it:arr) {
+            mpp[(long double)it]++;
+        }
+
+
+        int ans = 0;
+
+        for (auto it:mpp) {
+
+            int seq = 0;
+            int el = it.first;  
+
+            if (el==1) continue;
+            int times = 1;
+                while (seq < arr.size()) {
+                    long double val = pow(el , times);
+                    if (mpp.count(val) && mpp[val] >= 2) {
+                        seq++;
+                        times = times*2;
+                    } else if (mpp.count(val) && mpp[val] < 2) {
+                        
+                        //we found our peak;
+                        seq++;
+                        break;
+
+                    } else {
+                        break;
+                    }
+                }
+
+                ans = max(seq , ans);
+
+        }
+
+        cout << ans;
+
+        if (ans > 1) {
+
+            ans += (ans-1);
+
+        }
+
+        int one_freq = mpp[1];
+
+        if ((long long)mpp[1]%2 == 0) {
+            one_freq = mpp[1]-1;
+        } 
+
+        ans = max(ans , one_freq);
+
+        return ans;
+
+    }
+};
